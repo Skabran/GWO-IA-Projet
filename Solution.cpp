6fe7bf5b3850fdd2adf2ffc0e061a35dff1df73b
[Kevin] [Ruat] <[rukerdo99bg]@[gmail.com]>
2020-12-07 18:00:15 +0100

    avancement

--- Solution.cpp
+++ Solution.cpp
@@ -1,4 +1,20 @@
 #include "Solution.h"
 #include <vector>
 
+
+
+const Solution::Problem& pbm() const{
+    return _pbm;
+}
+
+
+double Solution::get_fitness() const{
+    return _current_fitness;
+}
+
+
+vector<double>& Solution::solution() const{
+    return _solution;
+}
+
 using namespace std;
